35b8a7cc7616ddaee8ed7cb9d07f9f73947472ad
Jack Dixon <jackaverydixon@gmail.com>
2026-02-17 16:11:59 -0500

    made modal look better

--- client/src/components/taskModal.jsx
+++ client/src/components/taskModal.jsx
@@ -1,11 +1,12 @@
 import getFormattedDate from "../utils/getFormattedDate";
 
 export default function TaskModal({ isOpen, onClose, onAddTask }) {
+  if (!isOpen) return null;
+
   function handleSubmit(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
     const formJson = Object.fromEntries(formData.entries());
-    console.log(formJson);
     const newTask = {
       title: formJson.title,
       start: formJson.date + "T" + formJson.startTime,
@@ -16,51 +17,49 @@ export default function TaskModal({ isOpen, onClose, onAddTask }) {
     onAddTask(newTask);
     onClose();
   }
-  if (!isOpen) return null;
 
   return (
     <div className="modal-overlay">
       <div className="modal-content">
         <p>New Task</p>
-        <form method="post" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <label>
             Title:
             <input type="text" placeholder="Title" name="title" />
           </label>
-          <br />
           <label>
             Date:
             <input
-              type="Date"
+              type="date"
               name="date"
               defaultValue={getFormattedDate()}
               min="2026-02-01"
               max="2100-12-30"
             />
           </label>
-          <br />
-          <label>
-            Start Time:
-            <input type="time" name="startTime" />
-          </label>
-          <br />
-          <label>
-            End Time:
-            <input type="time" name="endTime" />
-          </label>
-          <br />
+          <div className="time-row">
+            <label>
+              Start Time:
+              <input type="time" name="startTime" />
+            </label>
+            <label>
+              End Time:
+              <input type="time" name="endTime" />
+            </label>
+          </div>
           <label>
             All Day?
-            <input type="checkbox" name="allDay" value="false" />
+            <input type="checkbox" name="allDay" />
           </label>
-          <br />
           <label>
             Description:
             <textarea rows="4" cols="30" name="description" />
           </label>
           <div>
             <button type="submit">Add</button>
-            <button onClick={onClose}>Cancel</button>
+            <button type="button " onClick={onClose}>
+              Cancel
+            </button>
           </div>
         </form>
       </div>
