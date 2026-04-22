376c7fa66bbc82b40914f1d4be5abd99b0eda144
Pbierley <P.bierley@yahoo.com>
2026-04-22 17:57:32 -0400

    add monkey

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,3 +1,5 @@
+import happyMonkey from "../assets/happy-monkey.png";
+
 export default function NavBar({
   onLoginClick,
   onLogoutClick,
@@ -10,37 +12,46 @@ export default function NavBar({
       className="navbar"
       style={{ backgroundColor: Colors.secondary, color: Colors.secondaryText }}
     >
-      <button
-        className="color-button"
-        onClick={onColorClick}
-        style={{ backgroundColor: Colors.tertiary, color: Colors.tertiaryText }}
-      >
-        <i
-          className="fa-solid fa-gear nav-icon"
-          style={{
-            color: Colors.tertiaryText,
-          }}
-        />
-      </button>
-      <div className="nav-name">
-        <p>Monkey See Monkey Do</p>
-      </div>
-      <button
-        className="login-button"
-        onClick={authUser ? onLogoutClick : onLoginClick}
-        style={{ backgroundColor: Colors.tertiary, color: Colors.tertiaryText }}
-      >
-        {authUser ? (
-          `Log Out (${authUser.username})`
-        ) : (
+      <div className="nav-left">
+        <button
+          className="color-button"
+          onClick={onColorClick}
+          style={{ backgroundColor: Colors.tertiary, color: Colors.tertiaryText }}
+        >
           <i
-            className="fa-solid fa-user-circle nav-icon"
+            className="fa-solid fa-gear nav-icon"
             style={{
               color: Colors.tertiaryText,
             }}
           />
-        )}
-      </button>
+        </button>
+        <img
+          src={happyMonkey}
+          alt="monkey"
+          className="nav-monkey"
+        />
+      </div>
+      <div className="nav-name">
+        <p>Monkey See Monkey Do</p>
+      </div>
+      <div className="nav-right">
+        <button
+          className="login-button"
+          onClick={authUser ? onLogoutClick : onLoginClick}
+          style={{ backgroundColor: Colors.tertiary, color: Colors.tertiaryText }}
+        >
+          {authUser ? (
+            `Log Out (${authUser.username})`
+          ) : (
+            <i
+              className="fa-solid fa-user-circle nav-icon"
+              style={{
+                color: Colors.tertiaryText,
+              }}
+            />
+          )}
+        </button>
+      </div>
     </nav>
   );
 }
