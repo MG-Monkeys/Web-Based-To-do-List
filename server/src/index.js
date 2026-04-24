b3d48938ac8c3ba3d4c5b1c75eaa270c988a39af
DannyPoff31 <dannylpoff3@gmail.com>
2026-04-24 16:09:58 -0400

    added api to find user by username

--- server/src/index.js
+++ server/src/index.js
@@ -566,6 +566,25 @@ app.get("/users/:id", async function (req, res) {
   }
 });
 
+// GET /users/username/:username - Get a user by username
+app.get("/users/username/:username", async function (req, res) {
+  try {
+    const user = await User.findOne({username: req.params.username});
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    return res.json({
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  }
+  catch (error) {
+    console.error("Error fetching users: ", error);
+    res.status(500).json({ error: "Error fetching users" });
+  }
+});
+
 // GET /users/groups/:id - Get a user's groups by id
 app.get("/users/groups/:id", async function (req, res) {
   try {
