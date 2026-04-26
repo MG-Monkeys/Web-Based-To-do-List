72358c1b8d0ad1ad0a299408e4c48537552adf6e
Nate Brewer <119635918+brewern5@users.noreply.github.com>
2026-04-26 15:23:11 -0400

    Fixed merge issues

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -13,42 +13,6 @@ export default function NavBar({
       className="navbar"
       style={{ backgroundColor: Colors.secondary, color: Colors.secondaryText }}
     >
-      <button
-        className="color-button"
-        onClick={onColorClick}
-        style={{ backgroundColor: Colors.tertiary, color: Colors.tertiaryText }}
-      >
-        <i
-          className="fa-solid fa-gear nav-icon"
-          style={{
-            color: Colors.tertiaryText,
-          }}
-        />
-      </button>
-      <div className="nav-name">
-        <p>Monkey See Monkey Do</p>
-      </div>
-      <button
-        className="login-button"
-        onClick={onInboxClick}
-        style={{ backgroundColor: Colors.tertiary, color: Colors.tertiaryText }}
-      >
-        <i
-          className="fa-solid fa-solid fa-envelope nav-icon"
-          style={{
-            color: Colors.tertiaryText,
-          }}
-        />
-      </button>
-
-      <button
-        className="login-button"
-        onClick={authUser ? onLogoutClick : onLoginClick}
-        style={{ backgroundColor: Colors.tertiary, color: Colors.tertiaryText }}
-      >
-        {authUser ? (
-          `Log Out (${authUser.user.username})`
-        ) : (
       <div className="nav-left">
         <button
           className="color-button"
@@ -72,6 +36,20 @@ export default function NavBar({
         <p>Monkey See Monkey Do</p>
       </div>
       <div className="nav-right">
+
+      <button
+        className="login-button"
+        onClick={onInboxClick}
+        style={{ backgroundColor: Colors.tertiary, color: Colors.tertiaryText }}
+      >
+        <i
+          className="fa-solid fa-solid fa-envelope nav-icon"
+          style={{
+            color: Colors.tertiaryText,
+          }}
+        />
+      </button>
+
         <button
           className="login-button"
           onClick={authUser ? onLogoutClick : onLoginClick}
